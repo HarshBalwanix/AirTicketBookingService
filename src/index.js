937170a94e00f28805cbc523fbfb276d182193ab
Harsh Balwani <balwaniharsh1001@gmail.com>
2023-04-30 22:41:10 +0530

    Modfied for API Gateway

--- src/index.js
+++ src/index.js
@@ -12,8 +12,11 @@ const setupAndStartServer=()=> {
         app.use(bodyParser.json());
         app.use(bodyParser.urlencoded({extended:true}));
 
-        app.use('/api',apiRoutes);
-
+        app.use('/bookingservice/api',apiRoutes);
+        // app.get('/bookingservice/api/v1/home',(req,res)=>
+        // {
+        //     return res.json({message:'Hitting the booking service'});
+        // })
         app.listen(PORT,()=>{
             console.log("Server started at PORT",PORT);
 
